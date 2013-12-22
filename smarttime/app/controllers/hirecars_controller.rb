3dbc28a63386da6b2e2b85dd3e421b7655eb3dbc
christian schumacher <schumacherchristian85@web.de>
2013-12-22 18:30:45 +0100

    history fuer hirecars erstellt

--- smarttime/app/controllers/hirecars_controller.rb
+++ smarttime/app/controllers/hirecars_controller.rb
@@ -14,9 +14,7 @@ class HirecarsController < ApplicationController
 
   # GET /hirecars/new
   def new
-    logger.debug "!!!!!!!!!!!!!!!!!!!!! #{current_user]}"
     @hirecar = Hirecar.new
-    logger.debug "!!!!!!!!!!!!!!!!!!!!! #{current_user]}"
   end
 
   # GET /hirecars/1/edit
@@ -30,6 +28,7 @@ class HirecarsController < ApplicationController
     
     respond_to do |format|
       if @hirecar.save
+        History.create("action"=>"add","user_id" => current_user.id, "description" =>"Leihwagen wurde erstellt")
         format.html { redirect_to @hirecar, notice: 'Hirecar was successfully created.' }
         format.json { render action: 'show', status: :created, location: @hirecar }
       else
@@ -44,6 +43,7 @@ class HirecarsController < ApplicationController
   def update
     respond_to do |format|
       if @hirecar.update(hirecar_params)
+        History.create("action"=>"change","user_id" => current_user.id, "description" =>"Leihwagen wurde editiert")
         format.html { redirect_to @hirecar, notice: 'Hirecar was successfully updated.' }
         format.json { head :no_content }
       else
@@ -57,6 +57,7 @@ class HirecarsController < ApplicationController
   # DELETE /hirecars/1.json
   def destroy
     @hirecar.destroy
+    History.create("action"=>"del","user_id" => current_user.id, "description" =>"Leihwagen wurde geloescht")
     respond_to do |format|
       format.html { redirect_to hirecars_url }
       format.json { head :no_content }
